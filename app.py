b658d75924e7db1e9c40f0bdbbd387ac20fd8930
Rose Adam <rosedarline@gmail.com>
2020-01-27 21:58:43 -0500

    Re-style C

--- app.py
+++ app.py
@@ -21,13 +21,13 @@ class feedback(db.Model):
     __tablename__ = 'feedback'
     id = db.Column(db.Integer, primary_key=True)
     customer = db.Column(db.String(200), unique=True)
-    dealer = db.Column(db.String(200))
+    ships = db.Column(db.String(200))
     rating = db.Column(db.Integer)
     comments = db.Column(db.Text())
 
-    def __init__(self, customer, dealer, rating, comments):
+    def __init__(self, customer, ships, rating, comments):
         self.customer = customer
-        self.dealer = dealer
+        self.ships = ships
         self.rating = rating
         self.comments = comments
 
@@ -42,18 +42,18 @@ def index():
 def submit():
     if request.method == 'POST':
         customer = request.form['customer']
-        dealer = request.form['dealer']
+        ships = request.form['ships']
         rating = request.form['rating']
         comments = request.form['comments']
         reviews = db.session.query(feedback).all()
-        print(customer, dealer, rating, comments, reviews)
-        if customer == '' or dealer == '':
+        print(customer, ships, rating, comments, reviews)
+        if customer == '' or ships == '':
             return render_template('index.html', message='Please enter required fields', reviews=reviews)
         if db.session.query(feedback).filter(feedback.customer == customer).count() == 0:
-            data = feedback(customer, dealer, rating, comments)
+            data = feedback(customer, ships, rating, comments)
             db.session.add(data)
             db.session.commit()
-            send_mail(customer, dealer, rating, comments)
+            send_mail(customer, ships, rating, comments)
             return render_template('success.html')
         return render_template('index.html', message='You have already submitted feedback', reviews=reviews)
 
